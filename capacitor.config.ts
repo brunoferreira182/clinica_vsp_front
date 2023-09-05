9b5c98fd0fd9406f10bfdda21a274a4c49077c1b
Tiago Razera <tiago.razera@pelservicos.com.br>
2023-09-05 09:21:47 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'b3dev_clinica_vsp_app_v2',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
